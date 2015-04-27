e6c49d5cf4d3cef909cd4572e814fa1d7d454ca0
Justin Yoo <justin.yoo@aliencube.com>
2015-04-27 12:59:07 +1000

    Add test project

---
+++ SourceCodes/99_Tests/HtmlHelperExtended.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.HtmlHelper.Extended.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91e119dd-8501-422a-ad49-e4a26e74a7f6")]
