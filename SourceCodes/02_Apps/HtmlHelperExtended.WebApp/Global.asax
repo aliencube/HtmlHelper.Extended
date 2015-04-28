aa4f7388f84d765614693d91267cf6861a6b823e
Justin Yoo <justin.yoo@aliencube.com>
2015-04-28 22:09:30 +1000

    Add sample website app

---
+++ SourceCodes/02_Apps/HtmlHelperExtended.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aliencube.HtmlHelper.Extended.WebApp.MvcApplication" Language="C#" %>
---
+++ SourceCodes/02_Apps/HtmlHelperExtended.WebApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.HtmlHelper.Extended.WebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df0f7d53-7690-4778-9132-7837860d55b9")]
