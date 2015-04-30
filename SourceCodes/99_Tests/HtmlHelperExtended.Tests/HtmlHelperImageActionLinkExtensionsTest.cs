99ebf02de6c695bb552d90af0195fb53bfed1819
Justin Yoo <justin.yoo@aliencube.com>
2015-04-30 18:11:16 +1000

    Update HtmlHelperImageActionLinkExtensionsTest to use anonymous object

--- SourceCodes/99_Tests/HtmlHelperExtended.Tests/HtmlHelperImageActionLinkExtensionsTest.cs
+++ SourceCodes/99_Tests/HtmlHelperExtended.Tests/HtmlHelperImageActionLinkExtensionsTest.cs
@@ -26,80 +26,59 @@ namespace Aliencube.HtmlHelper.Extended.Tests
             this._htmlHelper = null;
         }
 
-        //[Test]
-        //[TestCase("http://google.com", "TestAction", null, null, null, null)]
-        //[TestCase("http://google.com", "TestAction", null, "id=1", null, null)]
-        //[TestCase("http://google.com", "TestAction", null, null, "title=TestTitle", null)]
-        //[TestCase("http://google.com", "TestAction", null, null, null, "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", null, "id=1", "title=TestTitle", null)]
-        //[TestCase("http://google.com", "TestAction", null, "id=1", null, "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", null, null, "title=TestTitle", "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", null, "id=1", "title=TestTitle", "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", "TestController", null, null, null)]
-        //[TestCase("http://google.com", "TestAction", "TestController", "id=1", null, null)]
-        //[TestCase("http://google.com", "TestAction", "TestController", null, "title=TestTitle", null)]
-        //[TestCase("http://google.com", "TestAction", "TestController", null, null, "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", "TestController", "id=1", "title=TestTitle", null)]
-        //[TestCase("http://google.com", "TestAction", "TestController", "id=1", null, "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", "TestController", null, "title=TestTitle", "class=class1 class2")]
-        //[TestCase("http://google.com", "TestAction", "TestController", "id=1", "title=TestTitle", "class=class1 class2")]
-        //public void GivenSrcHrefAndAttributes_Should_ReturnHtmlTagString(string src, string actionName, string controllerName, string routeValues, string htmlAttributes, string imageAttributes)
-        //{
-        //    var rValues = new { id = String.Empty };
-        //    if (!String.IsNullOrWhiteSpace(routeValues))
-        //    {
-        //        rValues.GetType().GetProperty("id").SetValue(rValues, routeValues.Split('=')[1]);
-        //    }
+        [Test]
+        [TestCase("http://google.com", "TestAction", null, null, null, null)]
+        [TestCase("http://google.com", "TestAction", null, "id=1", null, null)]
+        [TestCase("http://google.com", "TestAction", null, null, "title=TestTitle", null)]
+        [TestCase("http://google.com", "TestAction", null, null, null, "border=0")]
+        [TestCase("http://google.com", "TestAction", null, "id=1", "title=TestTitle", null)]
+        [TestCase("http://google.com", "TestAction", null, "id=1", null, "border=0")]
+        [TestCase("http://google.com", "TestAction", null, null, "title=TestTitle", "border=0")]
+        [TestCase("http://google.com", "TestAction", null, "id=1", "title=TestTitle", "border=0")]
+        [TestCase("http://google.com", "TestAction", "TestController", null, null, null)]
+        [TestCase("http://google.com", "TestAction", "TestController", "id=1", null, null)]
+        [TestCase("http://google.com", "TestAction", "TestController", null, "title=TestTitle", null)]
+        [TestCase("http://google.com", "TestAction", "TestController", null, null, "border=0")]
+        [TestCase("http://google.com", "TestAction", "TestController", "id=1", "title=TestTitle", null)]
+        [TestCase("http://google.com", "TestAction", "TestController", "id=1", null, "border=0")]
+        [TestCase("http://google.com", "TestAction", "TestController", null, "title=TestTitle", "border=0")]
+        [TestCase("http://google.com", "TestAction", "TestController", "id=1", "title=TestTitle", "border=0")]
+        public void GivenSrcHrefAndAttributes_Should_ReturnHtmlTagString(string src, string actionName, string controllerName, string routeValues, string htmlAttributes, string imageAttributes)
+        {
+            var rValues = new { id = !String.IsNullOrWhiteSpace(routeValues) ? routeValues.Split('=')[1] : null };
+            var hAttributes = new { title = !String.IsNullOrWhiteSpace(htmlAttributes) ? htmlAttributes.Split('=')[1] : null };
+            var iAttributes = new { border = !String.IsNullOrWhiteSpace(imageAttributes) ? imageAttributes.Split('=')[1] : null };
 
-        //    var hAttributes = new Dictionary<string, object>();
-        //    if (!String.IsNullOrWhiteSpace(htmlAttributes))
-        //    {
-        //        hAttributes.Add(htmlAttributes.Split('=')[0], htmlAttributes.Split('=')[1]);
-        //    }
+            var link = String.IsNullOrWhiteSpace(controllerName)
+                           ? this._htmlHelper.ImageActionLink(src, actionName, rValues, hAttributes, iAttributes)
+                           : this._htmlHelper.ImageActionLink(src, actionName, controllerName, rValues, hAttributes, iAttributes);
 
-        //    var iAttributes = new Dictionary<string, object>();
-        //    if (!String.IsNullOrWhiteSpace(imageAttributes))
-        //    {
-        //        iAttributes.Add(imageAttributes.Split('=')[0], imageAttributes.Split('=')[1]);
-        //    }
-        //    var link = String.IsNullOrWhiteSpace(controllerName)
-        //                   ? this._htmlHelper.ImageActionLink(src, actionName, rValues, hAttributes, iAttributes)
-        //                   : this._htmlHelper.ImageActionLink(src, actionName, controllerName, rValues, hAttributes, iAttributes);
-
-        //    link.ToHtmlString().Should().Contain("><img");
-        //    if (!String.IsNullOrWhiteSpace(controllerName))
-        //    {
-        //        if (!String.IsNullOrWhiteSpace(routeValues))
-        //        {
-        //            link.ToHtmlString().Should().MatchRegex("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/" + controllerName + "/" + actionName + "/" + rValues.id + "\"");
-        //        }
-        //        else
-        //        {
-        //            link.ToHtmlString().Should().MatchRegex("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/" + controllerName + "/" + actionName + "\"");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        if (!String.IsNullOrWhiteSpace(routeValues))
-        //        {
-        //            link.ToHtmlString().Should().MatchRegex("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/home/" + actionName + "/" + rValues.id + "\"");
-        //        }
-        //        else
-        //        {
-        //            link.ToHtmlString().Should().MatchRegex("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/home" + actionName + "\"");
-        //        }
-        //    }
-        //    link.ToHtmlString().Should().Contain("src=\"" + src + "\"");
-
-        //    foreach (var attribute in hAttributes)
-        //    {
-        //        link.ToHtmlString().Should().Contain(attribute.Key + "=\"" + attribute.Value + "\"");
-        //    }
-
-        //    foreach (var attribute in iAttributes)
-        //    {
-        //        link.ToHtmlString().Should().Contain(attribute.Key + "=\"" + attribute.Value + "\"");
-        //    }
-        //}
+            link.ToHtmlString().Should().Contain("><img");
+            if (!String.IsNullOrWhiteSpace(controllerName))
+            {
+                if (!String.IsNullOrWhiteSpace(routeValues))
+                {
+                    link.ToHtmlString().Should().Contain("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/" + controllerName + "/" + actionName + "/" + rValues.id + "\"");
+                }
+                else
+                {
+                    link.ToHtmlString().Should().Contain("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/" + controllerName + "/" + actionName + "\"");
+                }
+            }
+            else
+            {
+                if (!String.IsNullOrWhiteSpace(routeValues))
+                {
+                    link.ToHtmlString().Should().Contain("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/home/" + actionName + "/" + rValues.id + "\"");
+                }
+                else
+                {
+                    link.ToHtmlString().Should().Contain("href=\"" + MvcHelper.APP_PATH_MODIFIER + "/home/" + actionName + "\"");
+                }
+            }
+            link.ToHtmlString().Should().Contain("src=\"" + src + "\"");
+            link.ToHtmlString().Should().Contain("title=\"" + hAttributes.title + "\"");
+            link.ToHtmlString().Should().Contain("border=\"" + iAttributes.border + "\"");
+        }
     }
 }
